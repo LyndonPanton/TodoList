73a702d579242542d2a8fc8dc1ecc1ad19d3d0b8
Lyndon Panton <LyndonPanton@gmail.com>
2019-02-17 16:22:36 +0000

    Added input event listener and todo submission logic

--- js/app.js
+++ js/app.js
@@ -5,3 +5,12 @@ let copyrightElement = document.createElement("span");
 let copyrightString = `${year} | All Rights Reserved`;
 copyrightElement.textContent = copyrightString;
 copyright.appendChild(copyrightElement);
+
+let input = document.getElementById("newtodo");
+// When a character is entered into the input field
+input.addEventListener("keydown", function(e) {
+	// If the enter key is pressed and the input's value is not either empty or only whitespace
+	if (e.keyCode === 13 && this.value.trim() !== "") {
+
+	}
+});
